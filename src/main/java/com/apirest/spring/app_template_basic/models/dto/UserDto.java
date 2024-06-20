a177da1c5fb2e533df64b1d240a3951acea41d27
Rolando HR <rolando167@hotmail.com>
2024-06-20 10:08:18 +0200

    RH 20 - ca

---
+++ src/main/java/com/apirest/spring/app_template_basic/models/dto/UserDto.java
@@ -0,0 +1,18 @@
+package com.apirest.spring.app_template_basic.models.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data //@ToString, @EqualsAndHashCode, @Getter, @Setter, @RequiredArgsConstructor
+@Builder
+public class UserDto  implements Serializable {
+    private String name;
+    private String lastname;
+    private Double height;
+    private Date date_register;
+}
+
+//▶️ user-create-dto, user-list-dto ... multiple fields
---
+++ src/main/java/com/apirest/spring/app_template_basic/models/entities/Users.java
@@ -0,0 +1,21 @@
+package com.apirest.spring.app_template_basic.models.entities;
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Getter
+@Setter
+//@Data = (@ToString, @EqualsAndHashCode, @Getter, @Setter, @RequiredArgsConstructor)
+// 50% falta la persistencia ejem:  jkarta
+public class Users implements Serializable{
+    private Integer id;
+    private String name;
+    private String lastname;
+    private Double height;
+    private Date date_register;
+}
