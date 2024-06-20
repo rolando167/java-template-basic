5bf3b4900af67c761556c282907c738ccf541cb2
Rolando HR <rolando167@hotmail.com>
2024-06-20 09:04:29 +0200

    RH 20 - ca

--- src/main/java/com/apirest/spring/app_template_basic/Controllers/HomeController.java
+++ src/main/java/com/apirest/spring/app_template_basic/Controllers/HomeController.java
@@ -1,6 +1,12 @@
 package com.apirest.spring.app_template_basic.Controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +23,14 @@ public class HomeController {
 
     private int numero() {
         return 49;
+    }
+
+    @GetMapping("/ordenar")
+    public ResponseEntity<?> getSortNumber(@RequestBody Map<?,Object> param) {
+
+        Object datos = param.get("datos");
+        Map<String, Object> response = new HashMap<String, Object>();
+        response.put("England", datos);
+        return new ResponseEntity<>(response , HttpStatus.OK);
     }
 }
