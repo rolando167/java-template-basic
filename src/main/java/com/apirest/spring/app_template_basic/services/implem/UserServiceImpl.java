fe65d2688f4ecf18c36d31916d003d785625be0d
Rolando HR <rolando167@hotmail.com>
2024-06-22 10:34:44 +0200

    RH 20 - ca

--- src/main/java/com/apirest/spring/app_template_basic/services/implem/UserServiceImpl.java
+++ src/main/java/com/apirest/spring/app_template_basic/services/implem/UserServiceImpl.java
@@ -13,27 +13,6 @@ import com.apirest.spring.app_template_basic.services.interfaces.IUserService;
 @Service
 public class UserServiceImpl implements IUserService {
 
-    private List<Users> getUsers(){
-        Users user1 = new Users();
-        user1.setId(1);
-        user1.setName("Luis");
-        user1.setLastname("Martinez");
-        user1.setHeight(1.75);
-        user1.setDate_register(new java.util.Date());
-
-        Users user2 = new Users();
-        user2.setId(2);
-        user2.setName("Lola");
-        user2.setLastname("Toyota");
-        user2.setHeight(1.64);
-        user2.setDate_register(new java.util.Date());
-
-        List<Users> lista = new ArrayList<Users>();
-        lista.add(user1);
-        lista.add(user2);
-        return lista ;
-    }
-
     @Override
     public List<Users> getAll() {
         return getUsers();
@@ -106,6 +85,27 @@ public class UserServiceImpl implements IUserService {
             }
         }
         return exists;
+    }
+
+    private List<Users> getUsers(){
+        Users user1 = new Users();
+        user1.setId(1);
+        user1.setName("Luis");
+        user1.setLastname("Martinez");
+        user1.setHeight(1.75);
+        user1.setDate_register(new java.util.Date());
+
+        Users user2 = new Users();
+        user2.setId(2);
+        user2.setName("Lola");
+        user2.setLastname("Toyota");
+        user2.setHeight(1.64);
+        user2.setDate_register(new java.util.Date());
+
+        List<Users> lista = new ArrayList<Users>();
+        lista.add(user1);
+        lista.add(user2);
+        return lista ;
     }
 
 }
